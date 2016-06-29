d112fdf6626c34e8807acef7a5e1cf032b7d36a6
root <root@instance-1ill6rwh.(none)>
2016-06-29 15:28:47 +0800

    更新init脚本生成系统匹配 initctl文件

--- admin/daemontools-0.76/init.sh
+++ admin/daemontools-0.76/init.sh
@@ -16,19 +16,28 @@ else
 fi
 
 ROOT_DIR=${SELF_ABS}
-
 COMMAND=${ROOT_DIR}/command/
-echo -e  "#!/bin/sh \n" > ${ROOT_DIR}/linkedsee_svscan.conf
-echo "PATH=/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/X11R6/bin:$COMMAND " >> ${ROOT_DIR}/linkedsee_svscan.conf
+#根据路径写svscanboot
+echo "#!/bin/sh" > ${COMMAND}/svscanboot
+echo "PATH=/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/X11R6/bin:$COMMAND " >> ${COMMAND}/svscanboot
 
-echo "exec < /dev/null" >> ${ROOT_DIR}/linkedsee_svscan.conf
-echo  >> ${ROOT_DIR}/linkedsee_svscan.conf
-echo "exec > /dev/null" >> ${ROOT_DIR}/linkedsee_svscan.conf
-echo -e "exec 2> /dev/null">> ${ROOT_DIR}/linkedsee_svscan.conf
-echo  >> ${ROOT_DIR}/linkedsee_svscan.conf
-echo "svc -dx ${COMMAND}/services/* ${COMMAND}/services/*/log" >> ${ROOT_DIR}/linkedsee_svscan.conf
-echo  >> ${ROOT_DIR}/linkedsee_svscan.conf
-echo "env - PATH='\$PATH' svscan ${COMMAND}/service 2>&1 | \\" >> ${ROOT_DIR}/linkedsee_svscan.conf
-echo  >> ${ROOT_DIR}/linkedsee_svscan.conf
-echo "env - PATH='\$PATH' readproctitle service errors: ................................................................................................................................................................................................................................................................................................................................................................................................................ " >> ${ROOT_DIR}/linkedsee_svscan.conf
+echo "exec < /dev/null" >> ${COMMAND}/svscanboot
+echo "exec > /dev/null" >> ${COMMAND}/svscanboot
+echo -e "exec 2> /dev/null">> ${COMMAND}/svscanboot
+echo  >> ${COMMAND}/svscanboot
+echo "${COMMAND}/svc -dx ${ROOT_DIR}/services/* ${ROOT_DIR}/services/*/log" >> ${COMMAND}/svscanboot
+echo  >> ${COMMAND}/svscanboot
+echo "env - PATH=\$PATH ${COMMAND}/svscan ${ROOT_DIR}/service 2>&1 | \\" >> ${COMMAND}/svscanboot
+echo  >> ${COMMAND}/svscanboot
+echo "env - PATH='\$PATH' readproctitle service errors: ................................................................................................................................................................................................................................................................................................................................................................................................................ " >> ${COMMAND}/svscanboot
+
+if [ -d /etc/init ];then
+  cp -f ${COMMAND}/svscanboot /etc/init/
+fi
+
+#写linkedsee_svscan监护程序(由init进程监控)
+echo "start on runlevel [345]" > /etc/init/linkedsee_svscan.conf
+echo "respawn" >> /etc/init/linkedsee_svscan.conf
+echo "exec ${ROOT_DIR}/command/svscanboot" >> /etc/init/linkedsee_svscan.conf
+
 
